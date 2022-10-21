0f0e1435bfbe6eb00c0b7cdbe8893f5b6c2c161a
FilsenOr <58681024+DABO128@users.noreply.github.com>
2022-10-21 20:44:22 +0300

    Add files via upload

---
+++ Hafta_4/Hafta_04 _TOKENİZATİON.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Stopwords
+# 
+
+# In[44]:
+
+
+from nltk.corpus import stopwords
+stop_words=set(stopwords.words("Chinese"))
+print(stop_words)
+
+
+# # Removing Stopwords
+# 
+
+# In[18]:
+
+
+from nltk.tokenize import word_tokenize
+text="""Hello Mr. Smith, how are you doing today? The weather is great, and city is awesome.
+The sky is pinkish-blue. You shouldn't eat cardboard"""
+tokenized_sent=word_tokenize(text)
+
+filtered_sent=[]
+for w in tokenized_sent:
+    if w not in stop_words:
+        filtered_sent.append(w)
+print("Tokenized Sentence:",tokenized_sent)
+print("Filterd Sentence:",filtered_sent)
+
+
+# # Stemming
+
+# In[19]:
+
+
+# Stemming
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+
+stemmed_words=[]
+for w in filtered_sent:
+    stemmed_words.append(ps.stem(w))
+
+print("Filtered Sentence:",filtered_sent)
+print("Stemmed Sentence:",stemmed_words)
+
+
+# # Lemmatization
+
+# In[35]:
+
+
+#Lexicon Normalization
+#performing stemming and Lemmatization
+
+from nltk.stem.wordnet import WordNetLemmatizer
+lem = WordNetLemmatizer()
+
+from nltk.stem.porter import PorterStemmer
+stem = PorterStemmer()
+
+word = "Lemmatization"
+print("Lemmatized Word:",lem.lemmatize(word,"v"))
+print("Stemmed Word:",stem.stem(word))
+
+
+# # POS Tagging
+
+# In[21]:
+
+
+sent = "Albert Einstein was born in Ulm, Germany in 1879."
+
+
+# In[22]:
+
+
+tokens=nltk.word_tokenize(sent)
+print(tokens)
+
+
+# In[23]:
+
+
+nltk.pos_tag(tokens)
+
+
+# # NNP:Proper noun singular
+# 
+
+# # VBD : Verb past tense
+# 
+
+# # CD:Cardinal number
+# 
+
+# # VBN:Verb past participle
+
+# In[ ]:
+
+
+
+
